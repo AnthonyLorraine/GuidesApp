2fa8a710ec7e4adbfaa0fbc6e804b4dd94784909
Anthony Lorraine <az6155@gmail.com>
2023-12-28 23:10:12 +0800

    Registration page and implementation started

--- GuidesApp.Web/Controllers/AuthController.cs
+++ GuidesApp.Web/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 ﻿using GuidesApp.Web.Models;
 using GuidesApp.Web.Service.IService;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace GuidesApp.Web.Controllers
 {
@@ -20,6 +21,34 @@ namespace GuidesApp.Web.Controllers
         public IActionResult Login()
         {
             return View(new LoginRequestDto());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto loginRequestDto)
+        {
+            LoginResponseDto? loginResponseDto = new();
+            try
+            {
+                ResponseDto? responseDto = await _authService.LoginAsync(loginRequestDto);
+                if (responseDto != null && responseDto.IsSuccess)
+                {
+                    string? resultJson = Convert.ToString(responseDto.Result);
+                    if (!string.IsNullOrEmpty(resultJson))
+                    {
+                        loginResponseDto = JsonConvert.DeserializeObject<LoginResponseDto>(resultJson);
+                    }
+                    else
+                    {
+                        TempData["ErrorMessage"] = $"Failed to login. Response result contains no data.";
+                        throw new Exception("Response result contains no data.");
+                    }
+                }
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception)
+            {
+                return View();
+            }
         }
 
         [HttpGet]
--- GuidesApp.Web/Models/AuthDto/RegistrationRequestDto.cs
+++ GuidesApp.Web/Models/AuthDto/RegistrationRequestDto.cs
@@ -6,6 +6,7 @@ namespace GuidesApp.Web.Models
 		public string UserName { get; set; }
 		public string DisplayName { get; set; }
 		public string Password { get; set; }
+		public string PasswordConfirmation { get; set; }
 	}
 }
 
